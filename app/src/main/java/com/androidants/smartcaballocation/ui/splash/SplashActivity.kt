642238cb2b809890e3e1ba629100b4805885228d
Ansh <anshtandonat@gmail.com>
2024-01-17 17:36:19 +0530

    added animation

--- app/src/main/java/com/androidants/smartcaballocation/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/androidants/smartcaballocation/ui/splash/SplashActivity.kt
@@ -2,12 +2,15 @@ package com.androidants.smartcaballocation.ui.splash
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.androidants.smartcaballocation.ui.main.MainActivity
 import com.androidants.smartcaballocation.R
 import com.androidants.smartcaballocation.ui.authentication.register.RegisterActivity
 import dagger.hilt.android.AndroidEntryPoint
+import java.lang.Thread.sleep
 
 @AndroidEntryPoint
 class SplashActivity : AppCompatActivity() {
@@ -29,10 +32,12 @@ class SplashActivity : AppCompatActivity() {
     }
 
     private fun initAnimation(auth : Boolean) {
-        if ( auth )
-            startActivity(Intent(this , MainActivity::class.java))
-        else
-            startActivity(Intent(this , RegisterActivity::class.java))
-        finish()
+        Handler(Looper.getMainLooper()).postDelayed({
+            if ( auth )
+                startActivity(Intent(this , MainActivity::class.java))
+            else
+                startActivity(Intent(this , RegisterActivity::class.java))
+            finish()
+        }, 3700)
     }
 }
